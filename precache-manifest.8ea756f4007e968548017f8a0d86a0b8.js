9fa9b22ed2b81499a06a404d588dc23779aefc9d
pjbprogramming <pjbprogramming@gmail.com>
2022-02-10 14:37:29 +0530

    Updates

---
+++ precache-manifest.8ea756f4007e968548017f8a0d86a0b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c3ba7fdd38ef83cc0f79b32d8ed06b",
+    "url": "/texters/index.html"
+  },
+  {
+    "revision": "7fe05f6631eff965e61f",
+    "url": "/texters/static/css/main.96f5226e.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/texters/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "63fbe989101a4b7a3114",
+    "url": "/texters/static/js/3.13844b13.chunk.js"
+  },
+  {
+    "revision": "7fe05f6631eff965e61f",
+    "url": "/texters/static/js/main.d30b0737.chunk.js"
+  },
+  {
+    "revision": "d06ce340c3e9bac384c2",
+    "url": "/texters/static/js/runtime~main.94d636b0.js"
+  }
+]);
